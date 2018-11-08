8cb78141e6f3e8dbe8978c6433cb612160dc91e4
Jordan Talobre <jordan.talobre@centrale-marseille.fr>
2018-11-08 15:32:06 +0100

    volley

---
+++ app/src/main/java/com/example/jordan/projectapplication/Destination.java
@@ -0,0 +1,53 @@
+package com.example.jordan.projectapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class Destination extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destination);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        ImageView mImageView;
+        final TextView mTxtDisplay = (TextView) findViewById(R.id.txtDisplay);
+        String url = "http://www.rollingtown.com/?json=get_recent_posts";
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,new Response.Listener() {
+            @Override
+            public void onResponse(Object o) {
+            // Display the first 500 characters of the response string.
+                String response = (String)o;
+                mTxtDisplay.setText("Response is: "+ response.substring(0,500));
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTxtDisplay.setText("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+
+}
